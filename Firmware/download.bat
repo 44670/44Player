059741a68410a8499e541d6730858284745aea76
44670 <44670@users.noreply.github.com>
2022-11-20 18:45:11 +0800

    Update download.bat

--- Firmware/download.bat
+++ Firmware/download.bat
@@ -9,3 +9,6 @@ copy *.* ..\
 cd ..
 
 isd_download.exe -tonorflash -dev br21 -boot 0x2000 -div6 -wait 300  -f uboot.boot sdk.app bt_cfg.bin F_ASCII_S.PIX F_GB2312_S.PIX F_GB2312.TAB menu.res ui_sty.sty
+
+
+pause
